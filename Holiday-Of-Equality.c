197b979d29f39d004972bb28d8b572f3ec2e4c48
Shafayet Maruf <you@example.com>
2021-09-06 14:31:49 +0600

    800 difficulty level solves

---
+++ Holiday-Of-Equality.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	int nOfCitizen, largeNumber = 0, totalWelfareAmount = 0;
+	scanf("%d", &nOfCitizen);
+	int welfares[nOfCitizen]; 
+	for(int i = 0; i < nOfCitizen; i++){
+		scanf("%d", &welfares[i]);
+		totalWelfareAmount += welfares[i];
+		if(welfares[i] > largeNumber){
+			largeNumber = welfares[i];
+		}
+	}
+
+	printf("%d", (nOfCitizen * largeNumber) - totalWelfareAmount);
+	return 0;
+}
+
+//problem-link -> https://codeforces.com/problemset/problem/758/A
+//time-complexity -> O()
