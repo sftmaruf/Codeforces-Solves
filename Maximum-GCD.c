6b13e3e19310d4d11d90292eeb2ee71ec0f5bf24
Shafayet Maruf <shafayetmaruf@gmail.com>
2021-09-24 11:35:16 +0600

    altered variable name appropriately

--- Maximum-GCD.c
+++ Maximum-GCD.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main(){
-	int testCases, singleInt, greatestCommonDivisor;
+	int testCases, singleInt, maxGCD;
 	scanf("%d", &testCases);
 
 	while(testCases--){
 		scanf("%d", &singleInt);
 		if(singleInt % 2 == 0){
-			greatestCommonDivisor = singleInt / 2;
+			maxGCD = singleInt / 2;
 		}else{
-			greatestCommonDivisor = (singleInt - 1) / 2;
+			maxGCD = (singleInt - 1) / 2;
 		}
-		printf("%d\n", greatestCommonDivisor);
+		printf("%d\n", maxGCD);
 	}
 	return 0;
 }
