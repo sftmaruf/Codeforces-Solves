f4cf100598cd7e27bed38751fe350221ea4f7acb
Shafayet Maruf <shafayetmaruf@gmail.com>
2021-12-15 14:04:34 +0600

    800 difficulty level solves

---
+++ Again-Twenty-Five.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	int power;
+	scanf("%d", &power);
+	printf("25\n");
+	return 0;
+}
+
+//problem-link -> https://codeforces.com/problemset/problem/630/A
+//time-complexity -> O(1)
